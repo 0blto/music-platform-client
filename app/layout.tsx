0f11420a22aab43f0e34221a425e1d18c45dc3b0
drain shawty <hopegasgas@gmail.com>
2023-12-09 22:40:00 +0300

    aya

---
+++ app/layout.tsx
@@ -0,0 +1,26 @@
+import type { Metadata } from 'next'
+import { Figtree } from 'next/font/google'
+import './globals.css'
+
+import Sidebar from '@/components/Sidebar'
+
+const font = Figtree({ subsets: ['latin'] })
+
+export const metadata: Metadata = {
+  title: 'Coma te llama',
+  description: 'new school music-app',
+}
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body className={font.className}>
+        <Sidebar>{children}</Sidebar>
+      </body>
+    </html>
+  )
+}
